82d666d4d42e809885f447fbdf9c79f947b8afed
DoSomehow <329237665@qq.com>
2018-06-07 19:35:55 +0800

    Initial commit

---
+++ src/main/java/org/ms/myboot/MybootApplication.java
@@ -0,0 +1,20 @@
+package org.ms.myboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MybootApplication {
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello Spring Boot!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybootApplication.class, args);
+	}
+}
